a54fcc9759fbf67f4b0ec73d2afaa05ba465635a
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-01 12:50:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,4 +9,17 @@ else current_line = "The line is currently:"
 end
 end
 
-katz_deli = [ ]
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
