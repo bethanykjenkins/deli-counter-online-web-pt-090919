48e575650505677c71498020ca3a5c589db2ded6
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-01 12:50:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line (deli_line)
+  if deli_line.empty?
+  puts "The line is currently empty."
+else current_line = "The line is currently:"
+  deli_line.each.with_index(1) do |patron, i|
+    current_line << " #{i}. #{patron}"
+  end
+ puts current_line
+end
+end
+
+katz_deli = [ ]
